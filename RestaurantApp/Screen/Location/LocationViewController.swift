615793a22334d29cd848554912396cebd545bcda
Mohammed Badr <mohammed.badr20e@gmail.com>
2019-03-11 22:30:41 +0200

    Connecting LocationService to UI HalfWay

--- RestaurantApp/Screen/Location/LocationViewController.swift
+++ RestaurantApp/Screen/Location/LocationViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 class LocationViewController: UIViewController {
     
     @IBOutlet weak var locationView: LocationView!
+    var locationService: LocationService!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        locationView.didTapAllow = { [weak self] in
+            self?.locationService?.requestLocationAuthorization()
+        }
         // Do any additional setup after loading the view.
     }
 
