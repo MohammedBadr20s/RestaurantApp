d71fd177a9fd5601cfd07d3f79267c1f707cfb0f
Mohammed Badr <mohammed.badr20e@gmail.com>
2019-03-11 18:51:19 +0200

    Adding LocationService

--- RestaurantApp/Screen/BaseView.swift
+++ RestaurantApp/Screen/BaseView.swift
@@ -16,7 +16,7 @@ import UIKit
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
         self.configure()
     }
     func configure() {
