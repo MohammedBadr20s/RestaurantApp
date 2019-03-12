45f89dbc69fb33f1d365858241947d32bd613325
Mohammed Badr <mohammed.badr20e@gmail.com>
2019-03-12 22:37:27 +0200

    Connected The app to The Yelp APi

--- RestaurantApp/AppDelegate.swift
+++ RestaurantApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Moya
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,10 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let window = UIWindow()
     let locationService = LocationService()
     let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-
+    let service = MoyaProvider<YelpService.bussinessProvider>()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        service.request(.search(lat: 42, long: -71)) { (result) in
+            switch result {
+            case .success(let response):
+                print(try? JSONSerialization.jsonObject(with: response.data, options: []))
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
         switch locationService.status {
         case .notDetermined, .denied, .restricted:
             let locationViewController = storyBoard.instantiateViewController(withIdentifier: "locationViewController") as? LocationViewController
---
+++ RestaurantApp/Services/NetworkService.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkService.swift
+//  RestaurantApp
+//
+//  Created by Mohammed Badr on 2019-03-12.
+//  Copyright © 2019 Mohammed Badr. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+private let apiKey = "rJQczzCzdHLMFSF4ZQOdQU_aZDiy1_JOgtrMMe6HX8da9YXmjiDqKRi1vQiKCgD1rxR9omGQBlDXuTt55Mw7Zjro6RDyQnTQ9fKr8NtQ8moIHIAWA29VsuDrXRGIXHYx"
+
+enum YelpService {
+    enum bussinessProvider: TargetType {
+        
+        var baseURL: URL {
+            return URL (string: "https://api.yelp.com/v3/businesses")!
+        }
+        
+        var path: String {
+            switch self {
+            case .search:
+                return "/search"
+            }
+        }
+        
+        var method: Moya.Method {
+            return .get
+        }
+        
+        var sampleData: Data {
+            return Data()
+        }
+        
+        var task: Task {
+            switch self {
+            case let .search(lat,long):
+                return .requestParameters(parameters: ["latitude": lat, "longitude": long, "limit": 1], encoding: URLEncoding.queryString)
+            }
+        }
+        
+        var headers: [String : String]? {
+            return ["Authorization": "Bearer \(apiKey)"]
+        }
+        
+        case search (lat: Double, long: Double)
+    }
+}
